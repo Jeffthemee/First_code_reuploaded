e9ea928e4fd8f62d253fc5366d35a9c5ba19dcf3
joshkroker <57741393+joshkroker@users.noreply.github.com>
2019-11-13 20:47:24 -0800

    Add files via upload
    hi

---
+++ hi.py
@@ -0,0 +1,45 @@
+print ('+')
+a = input ('num')
+b = input ('num')
+aa = a + b
+print (aa)
+print ('-')
+s = input('num')
+v = input('num')
+ss = s - v
+print (ss)
+print ('*')
+d = input ('num')
+c = input ('num')
+cc = d * c
+print (cc)
+print ('<')
+f = input ('num')
+x = input ('num')
+xx = f < x
+print (xx)
+
+
+def repeat(param):pass
+print ('+')
+a = input ('num')
+b = input ('num')
+aa = a + b
+print (aa)
+print ('-')
+s = input('num')
+v = input('num')
+ss = s - v
+print (ss)
+print ('*')
+d = input ('num')
+c = input ('num')
+cc = d * c
+print (cc)
+print ('<')
+f = input ('num')
+x = input ('num')
+xx = f < x
+print (xx)
+while True: repeat(30)
+
